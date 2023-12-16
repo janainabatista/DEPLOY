6d006d9a6405c05008780722ad5266a0fc3af3c7
janainabatista <janainadutrabatista@gmail.com>
2023-12-15 22:37:15 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/',async(req,res)=>{
+    return res.json(' Api está ok')
+})
+const port = process.env.PORT||3000
+app.listen(3000,()=>{
+    console.log(process.env.JANAINA)
+})
